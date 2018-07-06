0c8f401aee08931b810a320368cc9d0b4be37be7
foux16 <foux16@gmail.com>
2018-07-06 09:29:10 -0300

    Atualização do metodo de subtração de background e adição da contagem e identificação de objetos

--- shapeDetector.py
+++ shapeDetector.py
@@ -1,81 +1,147 @@
-# import cv2
-# import numpy as np
-# import math
-# import datetime
-#
-# #Inicializando a Camera
-# cap = cv2.VideoCapture(0)
-# #background = cv2.createBackgroundSubtractorMOG2()
-# _,frame = cap.read()
-# avg = np.float32(frame)
-# #Rodando Loop para captura de frame
-# while(1):
-# #Leitura de Video
-#     _,frame = cap.read()
-# #Calculando mediana frame a frame
-#     cv2.accumulateWeighted(frame,avg,0.001)
-#     background = cv2.convertScaleAbs(avg)
-#
-# #Convertendo frame para escala de cinza
-#     background = cv2.cvtColor(background,cv2.COLOR_BGR2GRAY)
-#     grayFrame = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-#
-# #Reduzindo Ruidos com Filtro gaussiano
-#     grayFrame = cv2.GaussianBlur(grayFrame,(5,5),0)
-#     #background_mask = background.apply(frame)
-#
-# #SEGMENTAÇÃO DE IMAGEM
-#     foreground = cv2.absdiff(background,grayFrame)
-#
-# #Setando um valor de treshold
-#     _,thresh = cv2.threshold(foreground,127,255,cv2.THRESH_BINARY)
-#     thresh = cv2.dilate(thresh,None,iterations=2)
-#     img,contorno,_ = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_NONE)
-#     cv2.drawContours(img,contorno,-1,(0,255,0),3)
-#     cv2.imshow('mascara',img)
-#     cv2.imshow('contorno',frame)
-#
-# #fechando janela
-#     if cv2.waitKey(20) & 0xFF == ord('q'):
-#         break
-#
-# #Encerrando camera
-# cap.release()
-# cv2.destroyAllWindows()
-
 import cv2
 import numpy as np
 import math
 import datetime
 
+#inicialização de Variaveis Globais
+objetoLabel = 0
+celular = 0
+tablet = 0
+notebook = 0
+linhaReferencia = 100
+ThresholdBinarizacao = 70
+areaMinima = 1000
+areaMaxObjeto1 = 10000
+areaMinObjeto2 = 15000
+areaMaxObjeto2 = 30000
+areaMinObjeto3 = 40000
+areaMaxObjeto3 = 60000
+frameReferencia = None
+objNotAssigned = 0
+
+def shapeDraw(cnt):
+    cv2.drawContours(frame, contorno, -1, (0, 255, 0), 3)
+    #(x, y, w, h) = cv2.boundingRect(cnt)
+    #cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+    return
+
+
+def shapeIdentifier():
+    # Avalianando qual o objeto
+    if area > areaMinima and area < areaMaxObjeto1:
+        objeto = 1
+        print('Celular Identificado')
+    if area > areaMinObjeto2 and area < areaMaxObjeto2:
+        objeto = 2
+        print('Tablet Identificado')
+    if area > areaMinObjeto3 and area < areaMaxObjeto3:
+        objeto = 3
+        print('Computador Identificado')
+    else:
+        print('Objeto Nao Identificado')
+        objeto = 4
+    return(objeto)
+
+def contador(centroide,coordenadaLinha):
+    #if cv2.absdiff()
+    return
+
+#*************************************SUBTRAÇÂO DE BACKGROUND E IDENTIFICAÇÂO DE OBJETO**************************************************************
 #Inicializando a Camera
 cap = cv2.VideoCapture(0)
-firstFrame = None
+
+#forca a camera a ter resolucao 640x480
+cap.set(3,640)
+cap.set(4,480)
+
 #Capturando frame para adaptação da luz ambiente
-for i in range(0,20):
-    _,frame = cap.read()
+for cont in range(0,20):
+    ret,frame = cap.read()
 
 #Rodando Loop para captura de frame
-while(1):
-#Leitura de Video
-    _,frame = cap.read()
+while True:
+#Leitura de Video e determinação de parametros de altura e largura de acordo com a resolução
+    ret,frame = cap.read()
+    altura = np.size(frame, 0)
+    largura = np.size(frame, 1)
+
+#Caso não seja possivel obter o frame encerrar
+    if not ret: break
+
 #convertendo frame para tons de cinza(Necessario para encontrar os contornos)
-    frameGray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    frameCinza = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+
+#Convertendo frame para cor HSV(Necessario para nao desenhar contornos sobre cor Humana)
+    #framePele = cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
+
+#Valor hsv referente a cor da pele humana
+    #peleInferior = np.array([0,10,60])
+    #peleSuperior = np.array([35,180,255])
+
+#Limitando os valores da cor da pele
+    #mascara = cv2.inRange(framePele, peleInferior, peleSuperior)
+    #res = cv2.bitwise_and(frame,frame, mask= mascara)
+
 #Aplicando filtro gaussiano para diminuir o ruido da imagem
-    frameGray = cv2.GaussianBlur(frameGray,(15,15),0)
-#Comparando frames
-    if firstFrame is None:
-        firstFrame = frameGray
-    background = cv2.absdiff(firstFrame,frameGray)
+    frameBlur = cv2.GaussianBlur(frameCinza,(21,21),0)
+
+#Comparando frames para a subtração de background
+    if frameReferencia is None:
+        frameReferencia = frameBlur
+        continue
+    background = cv2.absdiff(frameReferencia,frameBlur)
+
 #Criando uma Mascara binaria(Mais facil computação e necessario para os contornos)
-    _,mask = cv2.threshold(background,127,255,cv2.THRESH_BINARY)
+    mask = cv2.threshold(background,ThresholdBinarizacao,255,cv2.THRESH_BINARY)[1]
+    mask = cv2.dilate(mask, None, iterations=2)
+
+#Desenhando Linha de referencia na tela
+    coordenadaLinhaY = int(altura / 2) - linhaReferencia
+    print(coordenadaLinhaY)
+    cv2.line(frame, (coordenadaLinhaY, 0), (coordenadaLinhaY, largura), (255, 0, 0), 2)
+
 #Encontrando os contornos
-    _,contorno,_ = cv2.findContours(mask,cv2.RETR_TREE,cv2.CHAIN_APPROX_NONE)
-#Desenhando os contornos na imagem
-    cv2.drawContours(frame,contorno,-1,(0,255,0),3)
-#Exibindo Video
-    cv2.imshow('mascara',mask)
-    cv2.imshow('frame',frame)
+    _,contorno,_ = cv2.findContours(mask.copy(),cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+#*********************************************************CONTAGEM DE OBJETOS E EXIBIÇÂO NA TELA************************************************************************
+#Definindo as propriedades dos contornos
+    for cnt in contorno:
+#Area e Perimetro
+        area = cv2.contourArea(cnt)
+        perimetro = cv2.arcLength(cnt, True)
+        #print('Area: ',area)
+        #print('Perimetro: ', perimetro)
+
+# Desenhando contornos apartir de uma dado tamanho(Pra ignorar pequenos contornos)
+        if contorno != None:
+            if area > areaMinima:
+                shapeDraw(cnt)
+                shapeIdentifier()
+                # Calculando Centroide do Objeto
+                M = cv2.moments(cnt)
+                cX = int(M['m10'] / M['m00'])
+                cY = int(M['m01'] / M['m00'])
+                centroide = (cX, cY)
+                #print('Centroide: ',centroide)
+                cv2.circle(frame,centroide,1,(0,0,0),5)
+# Verificar passagem do centroide pela linha de referencia
+
+# Contabilizar Objeto
+
+# Display do qtd
+
+        else:
+            #print('Nenhum Contorno foi Detectado \n')
+
+#Verificar se o objeto nao identificado passo multiplas vezes e oferecer cadastro: EXTRA
+        #objNotAssigned += 1
+
+#******************************************ARMAZENAMENTO DE INFORMAÇÔES NO BANCO DE DADOS**************************************************************
+
+
+#**********************************************************ENCERRANDO APLICAÇÃO************************************************************************
+
+    cv2.imshow('mascara', mask)
+    cv2.imshow('original', frame)
 
 #fechando janela
     if cv2.waitKey(20) & 0xFF == ord('q'):
