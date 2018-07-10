2b36dda63448c40f386ca527ae2bd56147a13470
foux16 <foux16@gmail.com>
2018-07-10 19:04:19 -0300

    Atualização do codigo. Adição da parte de contagem. Adição de informação na tela. Adição da parte de banco de dados. Faltando incorporar banco de dados com o restante do codigo

--- shapeDetector.py
+++ shapeDetector.py
@@ -1,28 +1,29 @@
 import cv2
 import numpy as np
+import sqlite3 as sq3
 import math
 import datetime
+import time
 
 #inicialização de Variaveis Globais
 objetoLabel = 0
 celular = 0
 tablet = 0
 notebook = 0
-linhaReferencia = 100
+objetoDesconhecido =0
+linhaReferencia = 200
 ThresholdBinarizacao = 70
-areaMinima = 1000
-areaMaxObjeto1 = 10000
-areaMinObjeto2 = 15000
-areaMaxObjeto2 = 30000
-areaMinObjeto3 = 40000
-areaMaxObjeto3 = 60000
+areaMinima = 200
+areaMaxObjeto1 = 1200
+areaMinObjeto2 = 1500
+areaMaxObjeto2 = 3000
+areaMinObjeto3 = 4000
+areaMaxObjeto3 = 6000
 frameReferencia = None
 objNotAssigned = 0
 
 def shapeDraw(cnt):
     cv2.drawContours(frame, contorno, -1, (0, 255, 0), 3)
-    #(x, y, w, h) = cv2.boundingRect(cnt)
-    #cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
     return
 
 
@@ -42,8 +43,19 @@ def shapeIdentifier():
         objeto = 4
     return(objeto)
 
-def contador(centroide,coordenadaLinha):
-    #if cv2.absdiff()
+def contador(cX,coordenadaLinhaX):
+    if (coordenadaLinhaX - cX) > 10:
+# impedindo que o objeto seja contado inumeras vezes
+        return 1
+    else:
+        return 0
+
+def displayContagem(frame,celular,tablet,notebook,objetoDesconhecido):
+    cv2.rectangle(frame, (485, 120), (635, 30), (0, 255, 0), 2)
+    cv2.putText(frame, "Celulares: {}".format(str(celular)), (490, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (250, 0, 1), 2)
+    cv2.putText(frame, "Tablets: {}".format(str(tablet)), (490, 70), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (250, 0, 1), 2)
+    cv2.putText(frame, "Notebooks: {}".format(str(notebook)), (490, 90), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (250, 0, 1), 2)
+    cv2.putText(frame, "Desconhcecido: {}".format(str(objetoDesconhecido)), (490, 110), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (250, 0, 1), 2)
     return
 
 #*************************************SUBTRAÇÂO DE BACKGROUND E IDENTIFICAÇÂO DE OBJETO**************************************************************
@@ -71,17 +83,6 @@ while True:
 #convertendo frame para tons de cinza(Necessario para encontrar os contornos)
     frameCinza = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
 
-#Convertendo frame para cor HSV(Necessario para nao desenhar contornos sobre cor Humana)
-    #framePele = cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
-
-#Valor hsv referente a cor da pele humana
-    #peleInferior = np.array([0,10,60])
-    #peleSuperior = np.array([35,180,255])
-
-#Limitando os valores da cor da pele
-    #mascara = cv2.inRange(framePele, peleInferior, peleSuperior)
-    #res = cv2.bitwise_and(frame,frame, mask= mascara)
-
 #Aplicando filtro gaussiano para diminuir o ruido da imagem
     frameBlur = cv2.GaussianBlur(frameCinza,(21,21),0)
 
@@ -96,9 +97,8 @@ while True:
     mask = cv2.dilate(mask, None, iterations=2)
 
 #Desenhando Linha de referencia na tela
-    coordenadaLinhaY = int(altura / 2) - linhaReferencia
-    print(coordenadaLinhaY)
-    cv2.line(frame, (coordenadaLinhaY, 0), (coordenadaLinhaY, largura), (255, 0, 0), 2)
+    coordenadaLinhaX = int(largura / 2) - linhaReferencia
+    cv2.line(frame, (coordenadaLinhaX, 0), (coordenadaLinhaX, largura), (255, 0, 0), 2)
 
 #Encontrando os contornos
     _,contorno,_ = cv2.findContours(mask.copy(),cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
@@ -108,36 +108,80 @@ while True:
 #Area e Perimetro
         area = cv2.contourArea(cnt)
         perimetro = cv2.arcLength(cnt, True)
-        #print('Area: ',area)
+        print('Area: ',area)
         #print('Perimetro: ', perimetro)
 
 # Desenhando contornos apartir de uma dado tamanho(Pra ignorar pequenos contornos)
         if contorno != None:
             if area > areaMinima:
                 shapeDraw(cnt)
-                shapeIdentifier()
-                # Calculando Centroide do Objeto
+#Definindo Objeto de acordo com suas propriedades
+                objetoLabel = shapeIdentifier()
+#Calculando Centroide do Objeto
                 M = cv2.moments(cnt)
                 cX = int(M['m10'] / M['m00'])
                 cY = int(M['m01'] / M['m00'])
                 centroide = (cX, cY)
                 #print('Centroide: ',centroide)
                 cv2.circle(frame,centroide,1,(0,0,0),5)
-# Verificar passagem do centroide pela linha de referencia
-
-# Contabilizar Objeto
-
-# Display do qtd
-
+# Verificar passagem do centroide pela linha de referencia e Contabilização do Objeto
+                if (contador(cX,coordenadaLinhaX)):
+                    if objetoLabel == 1:
+                        celular += 1
+                    if objetoLabel == 2:
+                        tablet += 1
+                    if objetoLabel == 3:
+                        notebook += 1
+                    else:
+                        objetoDesconhecido +=1
+                else:
+                    print('Aguardando Contagem')
         else:
-            #print('Nenhum Contorno foi Detectado \n')
+            print('Nenhum Contorno foi Detectado \n')
+
+#Display de informação de contagem
+    displayContagem(frame,celular,tablet,notebook,objetoDesconhecido)
 
 #Verificar se o objeto nao identificado passo multiplas vezes e oferecer cadastro: EXTRA
         #objNotAssigned += 1
 
 #******************************************ARMAZENAMENTO DE INFORMAÇÔES NO BANCO DE DADOS**************************************************************
+#inicializando tabela com dados pra database
+    conn = sq3.connect('produtos.db')
+    prod = conn.cursor()
 
+    prod.execute("""
+    CREATE TABLE clientes (
+    	id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    	tipo TEXT NOT NULL,
+    	area FLOAT,
+    	perimetro FLOAT,
+    	evento_em DATE NOT NULL,
+    	evento_type STRING(3) NOT NULL
+    );
+    """)
+    conn.close();
 
+    # inserindo dados na tabela
+
+    #obj é uma tupla com os dados
+
+    prod.executemany("""
+    INSERT INTO produtos (id, tipo, area, perimetro, evento_em, evento_type)
+    VALUES (?,?,?,?,?,?)
+    """, obj)
+    conn.commit()
+
+   #removendo dados da database usando o id como indicador
+    id_produto = 8
+
+    # excluindo um registro da tabela
+    prod.execute("""
+    DELETE FROM produtos
+    WHERE id = ?
+    """, (id_produto,))
+
+    conn.commit()
 #**********************************************************ENCERRANDO APLICAÇÃO************************************************************************
 
     cv2.imshow('mascara', mask)
